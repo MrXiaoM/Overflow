b3e675b929a62ae83e261e4483501e384e5c0c97
MrXiaoM <coolxiaom95@gmail.com>
2023-11-28 00:22:20 +0800

    add send message command

--- overflow-core/src/main/kotlin/top/mrxiaom/overflow/plugin/OverflowCoreAsPlugin.kt
+++ overflow-core/src/main/kotlin/top/mrxiaom/overflow/plugin/OverflowCoreAsPlugin.kt
@@ -2,7 +2,9 @@ package top.mrxiaom.overflow.plugin
 
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.runBlocking
+import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.command.*
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.permission.Permission
 import net.mamoe.mirai.console.permission.PermissionId
 import net.mamoe.mirai.console.plugin.Plugin
@@ -10,6 +12,7 @@ import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.description
 import net.mamoe.mirai.console.plugin.description.PluginDependency
 import net.mamoe.mirai.console.plugin.description.PluginDescription
 import net.mamoe.mirai.console.plugin.loader.PluginLoader
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.console.util.SemVersion
 import top.mrxiaom.overflow.BuildConstants
 import top.mrxiaom.overflow.Overflow
@@ -28,19 +31,51 @@ internal object OverflowCoreAsPlugin : Plugin, CommandOwner {
         return ConsoleCommandOwner.permissionId(name)
     }
 
+    @OptIn(ConsoleExperimentalApi::class)
     suspend fun onEnable() {
         OnebotMessages.registerSerializers()
         Overflow.instance.start()
 
         // keep a command register example here
-        /*
-        object : SimpleCommand(
+
+        object : CompositeCommand(
             owner = this,
             primaryName = "overflow",
-            secondaryNames = arrayOf()
+            secondaryNames = arrayOf(),
         ) {
+            @SubCommand
+            suspend fun CommandSender.group(
+                @Name("群号") groupId: Long,
+                @Name("消息")vararg message: String
+            ) {
+                val bot = Bot.instances.firstOrNull() ?: return Unit.also {
+                    sendMessage("至少有一个Bot在线才能执行该命令")
+                }
+                val group = bot.groups[groupId] ?: return Unit.also {
+                    sendMessage("找不到群 $groupId")
+                }
+                // TODO: 用更简洁的方法反序列化消息
+                val messageChain = OnebotMessages.deserializeFromOneBotJson(bot, message.joinToString(" "))
+                group.sendMessage(messageChain)
+                sendMessage("消息发送成功")
+            }
+            @SubCommand
+            suspend fun CommandSender.friend(
+                @Name("好友QQ") friendId: Long,
+                @Name("消息") vararg message: String
+            ) {
+                val bot = Bot.instances.firstOrNull() ?: return Unit.also {
+                    sendMessage("至少有一个Bot在线才能执行该命令")
+                }
+                val friend = bot.friends[friendId] ?: return Unit.also {
+                    sendMessage("找不到好友 $friendId")
+                }
+                // TODO: 用更简洁的方法反序列化消息
+                val messageChain = OnebotMessages.deserializeFromOneBotJson(bot, message.joinToString(" "))
+                friend.sendMessage(messageChain)
+                sendMessage("消息发送成功")
+            }
         }.register()
-         */
     }
 
     internal object TheLoader : PluginLoader<Plugin, PluginDescription> {
