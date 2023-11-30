137c2c95fddcce5f893e064b6af75d37649e6b74
MrXiaoM <coolxiaom95@gmail.com>
2023-11-30 19:17:39 +0800

    log unknown message

--- overflow-core/src/main/kotlin/top/mrxiaom/overflow/internal/message/data/UnknownMessage.kt
+++ overflow-core/src/main/kotlin/top/mrxiaom/overflow/internal/message/data/UnknownMessage.kt
@@ -5,12 +5,24 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import net.mamoe.mirai.message.data.MessageContent
+import net.mamoe.mirai.utils.createFileIfNotExists
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+private fun currentTime() = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
 
 @Serializable
 data class UnknownMessage(
     val type: String,
     val data: JsonObject
 ) : MessageContent {
+    init {
+        val logFile = File("logs/unknown_messages.log")
+        logFile.createFileIfNotExists()
+        logFile.appendText("${currentTime()} I/UnknownMessage: ${contentToString()}\n")
+    }
+    
     override fun contentToString(): String {
         data.toString()
         return "[overflow,unknown:$type,${Json.encodeToString(data)}]"
