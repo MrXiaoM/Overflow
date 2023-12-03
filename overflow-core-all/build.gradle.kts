e57a03babc5de7d0c2d3e0d02a8c48cbc9dbae70
MrXiaoM <coolxiaom95@gmail.com>
2023-12-03 14:20:54 +0800

    relocate dependencies

--- overflow-core-all/build.gradle.kts
+++ overflow-core-all/build.gradle.kts
@@ -13,3 +13,15 @@ dependencies {
     api(project(":overflow-core-api"))
     api(project(":overflow-core"))
 }
+
+tasks {
+    shadowJar {
+        mapOf(
+            "com.google.gson" to "gson",
+            "org.java_websocket" to "websocket",
+            "io.netty" to "netty"
+        ).forEach { (original, target) ->
+            relocate(original, "top.mrxiaom.overflow.internal.deps.$target")
+        }
+    }
+}
