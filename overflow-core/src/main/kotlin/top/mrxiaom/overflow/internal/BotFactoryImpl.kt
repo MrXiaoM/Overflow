45f03a843b402955eb281bb881007b28c8512685
MrXiaoM <coolxiaom95@gmail.com>
2023-12-16 16:42:42 +0800

    clean code

--- overflow-core/src/main/kotlin/top/mrxiaom/overflow/internal/BotFactoryImpl.kt
+++ overflow-core/src/main/kotlin/top/mrxiaom/overflow/internal/BotFactoryImpl.kt
@@ -10,22 +10,19 @@ import top.mrxiaom.overflow.internal.contact.BotWrapper
 object BotFactoryImpl : BotFactory {
     internal var internalBot: cn.evolvefield.onebot.client.core.Bot? = null
     val bot: cn.evolvefield.onebot.client.core.Bot
-        get() = top.mrxiaom.overflow.internal.BotFactoryImpl.internalBot!!
+        get() = internalBot!!
 
-    override fun newBot(qq: Long, passwordMd5: ByteArray, configuration: BotConfiguration): Bot =
-        top.mrxiaom.overflow.internal.BotFactoryImpl.end(qq, configuration)
+    override fun newBot(qq: Long, passwordMd5: ByteArray, configuration: BotConfiguration): Bot = end(qq, configuration)
 
-    override fun newBot(qq: Long, password: String, configuration: BotConfiguration): Bot =
-        top.mrxiaom.overflow.internal.BotFactoryImpl.end(qq, configuration)
+    override fun newBot(qq: Long, password: String, configuration: BotConfiguration): Bot = end(qq, configuration)
 
-    override fun newBot(qq: Long, authorization: BotAuthorization, configuration: BotConfiguration): Bot =
-        top.mrxiaom.overflow.internal.BotFactoryImpl.end(qq, configuration)
+    override fun newBot(qq: Long, authorization: BotAuthorization, configuration: BotConfiguration): Bot = end(qq, configuration)
 
     private fun end(qq: Long, configuration: BotConfiguration): Bot {
-        if (top.mrxiaom.overflow.internal.BotFactoryImpl.internalBot != null) runBlocking {
-            val data = top.mrxiaom.overflow.internal.BotFactoryImpl.internalBot!!.getLoginInfo().data
+        if (internalBot != null) runBlocking {
+            val data = internalBot!!.getLoginInfo().data
             if (data.userId == qq) {
-                Bot.getInstanceOrNull(qq) ?: BotWrapper.wrap(top.mrxiaom.overflow.internal.BotFactoryImpl.internalBot!!, configuration)
+                Bot.getInstanceOrNull(qq) ?: BotWrapper.wrap(internalBot!!, configuration)
             } else null
         }?.also { return it }
         throw UnsupportedOperationException("溢出核心已委托远程实现接管了账户管理，mirai 框架端没有登录机器人的职责")
