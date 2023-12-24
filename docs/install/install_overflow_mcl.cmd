7f5e2961d11237a8d9f5b07bc19d8274af21a9ab
MrXiaoM <coolxiaom95@gmail.com>
2023-12-24 13:50:47 +0800

    update mcl install script

--- docs/install/install_overflow_mcl.cmd
+++ docs/install/install_overflow_mcl.cmd
@@ -1,3 +1,4 @@
-mcl --remove-package net.mamoe:mirai-core-all
-mcl --update-package top.mrxiaom:overflow-core-all --channel maven-stable --type libs --version 2.16.0
+@echo off
+cmd /c mcl --remove-package net.mamoe:mirai-core-all
+cmd /c mcl --update-package top.mrxiaom:overflow-core-all --channel maven-snapshots --type libs --version 2.16.0
 pause
--- docs/install/install_overflow_mcl.sh
+++ docs/install/install_overflow_mcl.sh
@@ -1,2 +1,2 @@
 ./mcl --remove-package net.mamoe:mirai-core-all
-./mcl --update-package top.mrxiaom:overflow-core-all --channel maven-stable --type libs --version 2.16.0
+./mcl --update-package top.mrxiaom:overflow-core-all --channel maven-snapshots --type libs --version 2.16.0
