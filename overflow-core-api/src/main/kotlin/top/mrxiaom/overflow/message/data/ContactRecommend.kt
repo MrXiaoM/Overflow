73d2a12273f92216b3a2021e0b02904cc0c3f87e
人間工作 <coolxiaom95@gmail.com>
2024-01-22 23:37:23 +0800

    no `type`

--- overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/message/data/ContactRecommend.kt
+++ overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/message/data/ContactRecommend.kt
@@ -18,22 +18,22 @@ public data class ContactRecommend(
     /**
      * 联系人类型
      */
-    public val type: ContactType,
+    public val contactType: ContactType,
     public val id: Long
 ) : MessageContent, ConstrainSingle, CodableMessage {
     private val _contentValue: String by lazy(LazyThreadSafetyMode.NONE) {
-        val typeString = when(type) {
+        val typeString = when(contactType) {
             ContactType.Group -> "群聊"
             ContactType.Private -> "好友"
         }
         "[推荐$typeString]$id"
     }
-    public override fun toString(): String = "[overflow:contact,$type,$id]"
+    public override fun toString(): String = "[overflow:contact,$contactType,$id]"
     public override fun contentToString(): String = _contentValue
 
     @MiraiExperimentalApi
     override fun appendMiraiCodeTo(builder: StringBuilder) {
-        builder.append("[mirai:contact:").append(type.name.lowercase()).append(",").append(id).append("]")
+        builder.append("[mirai:contact:").append(contactType.name.lowercase()).append(",").append(id).append("]")
     }
 
     /**
