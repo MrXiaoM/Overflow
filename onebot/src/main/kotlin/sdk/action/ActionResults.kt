ec4311fe431ea6e6a2175798865c828dac549335
MrXiaoM <coolxiaom95@gmail.com>
2024-05-31 01:12:38 +0800

    move all basic action result into one file

---
+++ onebot/src/main/kotlin/sdk/action/ActionResults.kt
@@ -0,0 +1,35 @@
+package cn.evolvefield.onebot.sdk.action
+
+import com.google.gson.annotations.SerializedName
+import java.util.LinkedList
+
+data class ActionRaw(
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("retCode")
+    val retCode: Int,
+    @SerializedName("echo")
+    val echo: String? = null
+)
+
+data class ActionData<T : Any> (
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("retcode")
+    val retCode: Int,
+    @SerializedName("data")
+    val data: T,
+    @SerializedName("echo")
+    val echo: String? = null
+)
+
+data class ActionList<T : Any>(
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("retcode")
+    val retCode: Int,
+    @SerializedName("data")
+    val data: LinkedList<T>,
+    @SerializedName("echo")
+    val echo: String? = null
+)
