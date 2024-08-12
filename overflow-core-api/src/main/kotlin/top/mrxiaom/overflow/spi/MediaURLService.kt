15504a5efb98df9c9546622a0725cd7ef7347141
人間工作 <coolxiaom95@gmail.com>
2024-08-12 13:23:47 +0800

    [skip ci] MediaURLService comments

--- overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/spi/MediaURLService.kt
+++ overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/spi/MediaURLService.kt
@@ -8,12 +8,30 @@ import net.mamoe.mirai.spi.BaseService
 import net.mamoe.mirai.spi.SpiServiceLoader
 import top.mrxiaom.overflow.contact.RemoteBot
 
+/**
+ * 媒体消息下载链接获取服务
+ */
 interface MediaURLService : BaseService {
 
+    /**
+     * 获取图片链接
+     *
+     * @return 图片链接，返回 null 代表不进行处理，交给其它服务或 Overflow 自带服务处理
+     */
     suspend fun queryImageUrl(bot: RemoteBot, image: Image): String?
 
+    /**
+     * 获取语音链接
+     *
+     * @return 语音链接，返回 null 代表不进行处理，交给其它服务或 Overflow 自带服务处理
+     */
     fun queryAudioUrl(audio: Audio): String?
 
+    /**
+     * 获取视频链接
+     *
+     * @return 视频链接，返回 null 代表不进行处理，交给其它服务或 Overflow 自带服务处理
+     */
     fun queryVideoUrl(video: ShortVideo): String?
 
     companion object {
