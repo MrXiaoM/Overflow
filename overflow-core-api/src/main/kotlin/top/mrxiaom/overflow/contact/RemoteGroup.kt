293c0142cdddbf4b56c6b3986ce39e6b94ca9bca
人間工作 <coolxiaom95@gmail.com>
2025-03-31 23:30:40 +0800

    add remote contact cast

--- overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/contact/RemoteGroup.kt
+++ overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/contact/RemoteGroup.kt
@@ -10,4 +10,16 @@ interface RemoteGroup {
     suspend fun updateGroupMemberList(): ContactList<NormalMember>
     @JvmBlockingBridge
     suspend fun updateAnnouncements(): Announcements
+
+    companion object {
+        /**
+         * 尝试将 mirai Group 转换为 Overflow RemoteGroup
+         *
+         * 当类型不正确时，或没有 Overflow 实现时，将会抛出异常
+         */
+        @JvmStatic
+        @get:Throws(ClassNotFoundException::class, ClassCastException::class)
+        val Group.asRemoteGroup
+            get() = this as RemoteGroup
+    }
 }
--- overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/contact/RemoteUser.kt
+++ overflow-core-api/src/main/kotlin/top/mrxiaom/overflow/contact/RemoteUser.kt
@@ -1,5 +1,21 @@
 package top.mrxiaom.overflow.contact
 
+import net.mamoe.mirai.Bot
+import net.mamoe.mirai.contact.User
+import kotlin.jvm.Throws
+
 interface RemoteUser {
     val onebotData: String
+
+    companion object {
+        /**
+         * 尝试将 mirai User 转换为 Overflow RemoteUser
+         *
+         * 当类型不正确时，或没有 Overflow 实现时，将会抛出异常
+         */
+        @JvmStatic
+        @get:Throws(ClassNotFoundException::class, ClassCastException::class)
+        val User.asRemoteUser
+            get() = this as RemoteUser
+    }
 }
