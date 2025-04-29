4a721eeee59bfea8d79138dd926ffca72ec030af
人間工作 <coolxiaom95@gmail.com>
2025-04-29 18:49:12 +0800

    set version to 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 Helper.proj = rootProject
 group = "top.mrxiaom.mirai"
 
-val overflowVersion = "1.0.4".ext("overflowVersion")
+val overflowVersion = "1.0.5".ext("overflowVersion")
 val miraiVersion = "2.16.0".ext("miraiVersion")
 
 var commitHash = "local"
